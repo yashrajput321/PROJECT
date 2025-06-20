f052fd0e3b7f2fc25f671060610c6e4ba7531d27
yash rajput <rajputyash774@gmail.com>
2025-06-20 10:57:16 +0530

    commit

---
+++ E-commerce/src/Components/Hero.jsx
@@ -0,0 +1,19 @@
+import React from 'react'
+
+const Hero = () => {
+  return (
+    <div className='flex flex-col sm:flex-row border-gray-400'>
+        {/* Hero Left Side */}
+        <div className='w-full sm:w-1/2 flex items-center justify-center py-10 sm:py-0'>
+            <div className='text-[#414141]'>
+                <div className='flex items-center gap-2'>
+                    <p className='w-8 md:w-11 h-[2px] bg-[414141]'></p>
+                    <p className='font-medium text-sm md:text-base'>OUR BESTSELLER</p>
+                </div>
+            </div>
+        </div>
+    </div>
+  )
+}
+
+export default Hero
--- E-commerce/src/pages/Home.jsx
+++ E-commerce/src/pages/Home.jsx
@@ -1,8 +1,9 @@
 import React from 'react'
+import Hero from '../Components/Hero'
 
 const Home = () => {
   return (
-    <div>Welcome to CheemStore</div>
+    <Hero/>
   )
 }
 
