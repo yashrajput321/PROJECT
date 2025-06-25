4c1736c6104e7576b7f4221eae205ceb7e893bc9
yash rajput <rajputyash774@gmail.com>
2025-06-25 08:10:32 +0530

    commit

--- E-commerce/src/pages/About.jsx
+++ E-commerce/src/pages/About.jsx
@@ -1,9 +1,44 @@
 //src/pages/About
 import React from 'react'
+import Title from '../Components/Title'
+import { assets } from '../assets/frontend_assets/assets'
+import NewsletterBox from '../Components/NewsletterBox'
 
 const About = () => {
   return (
-    <div>About</div>
+    <div>
+      <div className='text-2xl text-center pt-8 border-t'>
+        <Title text1={'ABOUT'} text2={'US'}/>
+      </div>
+      <div className='my-10 flex flex-col md:flex-row gap-16'>
+        <img className='w-full md:max-w-[450px]' src={assets.about_img} alt="" />
+        <div className='flex flex-col justify-center gap-6 md:w-2/4 text-gray-600'>
+          <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Sit magnam ullam praesentium illo voluptas repellat tempora. Atque, delectus. Est, quisquam eius sed rem corporis laboriosam quae amet explicabo perspiciatis provident aliquam quas laudantium nemo.</p>
+          <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Deleniti necessitatibus accusantium repellat quas sint. Iure velit quidem saepe minima, reiciendis odit omnis beatae nobis mollitia sequi atque quia nisi, ducimus, dicta doloribus quibusdam? Aperiam, quidem provident ratione perferendis dolores maiores. Neque modi quae magnam tempora.</p>
+          <b>Our Mission</b>
+          <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Ratione eum mollitia nobis! Molestias, aliquid dolor quidem modi voluptatem eum temporibus non consequuntur maiores, possimus pariatur eveniet laborum rem? Unde totam modi, placeat repudiandae dolorem aliquid odit.</p>
+        </div>
+      </div>
+      <div className='text-xl py-4'>
+        <Title text1={'WHY'} text2={'CHOOSE US'} />
+      </div>
+      <div className='flex flex-col md:flex-row text-sm md-20 mb-20'>
+        <div className='border px-10 md:px-16 py-8 sm:py-20 flex flex-col gap-5'>
+          <b>Quality Assurance:</b>
+          <p className='text-gray-600'>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Similique, perspiciatis maxime explicabo, quasi odit non nemo accusamus voluptatem quo totam saepe. Porro quo dolore tenetur doloribus veritatis nobis soluta corporis quos ea iusto et nemo, laudantium odio totam natus quam.</p>
+        </div>
+        <div className='border-t border-b px-10 md:px-16 py-8 sm:py-20 flex flex-col gap-5'>
+          <b>Convenience:</b>
+          <p className='text-gray-600'>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Similique, perspiciatis maxime explicabo, quasi odit non nemo accusamus voluptatem quo totam saepe. Porro quo dolore tenetur doloribus veritatis nobis soluta corporis quos ea iusto et nemo, laudantium odio totam natus quam.</p>
+        </div>
+        <div className='border px-10 md:px-16 py-8 sm:py-20 flex flex-col gap-5'>
+          <b>Exceptional Customer Service:</b>
+          <p className='text-gray-600'>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Similique, perspiciatis maxime explicabo, quasi odit non nemo accusamus voluptatem quo totam saepe. Porro quo dolore tenetur doloribus veritatis nobis soluta corporis quos ea iusto et nemo, laudantium odio totam natus quam.</p>
+        </div>
+      </div>
+
+      <NewsletterBox/>
+    </div>
   )
 }
 
--- E-commerce/src/pages/Login.jsx
+++ E-commerce/src/pages/Login.jsx
@@ -1,9 +1,35 @@
 //src/pages/Login
-import React from 'react'
+import React, { useState } from 'react'
 
 const Login = () => {
+
+  const [currentState,setCurrentState] = useState('Login')
+
+  const onSubmitHandle= async (event)=>{
+
+    event.preventDefault()
+
+  }
+
+
   return (
-    <div>Login</div>
+    <form onSubmit={onSubmitHandle} className='flex flex-col items-center w-[90%] sm:max-w-96 m-auto mt-14 gap-4 text-gray-800 '>
+      <div className='inline-flex items-center gap-2 mb-2 mt-10 '>
+        <p className='prata-regular text-3xl'>{currentState}</p>
+        <hr className='border-none h-[1.5px] w-8 bg-gray-800' />
+      </div>
+      {currentState==='Login'?'':<input type="text" placeholder='name' className='w-full px-3 py-2 border border-gray-800' />}
+      <input type="email" placeholder='e-mail' className='w-full px-3 py-2 border border-gray-800'  required/>
+      <input type="password" placeholder='password' className='w-full px-3 py-2 border border-gray-800' required/>
+      <div className='w-full flex justify-between text-sm mt-[-8px]'>
+        {currentState==='Login'?
+      <p onClick={()=>{setCurrentState('Sign Up')}} className='cursor-pointer'>Create Account</p>:
+      <p onClick={()=>{setCurrentState('Login')}} className='cursor-pointer'>Already have an Account</p>
+      }
+      {currentState==='Login'?<p className='cursor-pointer'>Forgot Password?</p>:''}  
+      </div>
+      <button type='submit' className='bg-black text-white font-light px-8 py-2 mt-4'>{currentState=='Login'?'Sign In':'Sign Up'}</button>
+    </form>
   )
 }
 
