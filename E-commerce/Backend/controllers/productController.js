f599608e2737405c51371bb006fdbc4019b10299
yash rajput <rajputyash774@gmail.com>
2025-07-03 13:13:10 +0530

    commit

--- E-commerce/Backend/controllers/productController.js
+++ E-commerce/Backend/controllers/productController.js
@@ -1,21 +1,63 @@
+import {v2 as cloudinary} from 'cloudinary'
+import productModel from '../models/productModel.js'
+
 // function for add product
 const addProduct = async (req,res) =>{
     try {
         
         const {name,description,price,category,subCategory,sizes,bestseller} = req.body
-        const image1 = req.files.image1[0]
-        const image2 = req.files.image2[0]
-        const image3 = req.files.image3[0]
-        const image4 = req.files.image4[0]
+        const image1 = req.files.image1 && req.files.image1[0]
+        const image2 = req.files.image2 && req.files.image2[0]
+        const image3 = req.files.image3 && req.files.image3[0]
+        const image4 = req.files.image4 && req.files.image4[0]
 
-    } catch (error) {
+        const images = [image1,image2,image3,image4].filter((item)=>{
+            item != undefined
+        })
+
+        let imageUrl = await Promise.all(
+            images.map(async(item)=>{
+                let result = await cloudinary.uploader.upload(item.path,{resource_type:'image'})
+                return result.secure_url
+            })
+        )
+
+        const productData = {
+            name,
+            description,
+            category,
+            subCategory,
+            sizes:JSON.parse(sizes),
+            price:Number(price),
+            bestseller: bestseller==="true"?true : false,
+            image:imageUrl,
+            date:Date.now()
+        }
+
+        console.log(productData)
+
+        const product = new productModel(productData)
+        await product.save()
         
+        res.json({success:true,message:"Product Added"})
+    } catch (error) {
+        console.log("Error adding product:", error);
+        res.json({success:false,message:error.message})
     }
 
 }
 
 // functiomn for list product
 const listProduct = async (req,res) =>{
+    try {
+
+        const products = await productModel.find({})
+        res.json({success:true,products})
+        
+    } catch (error) {
+        console.log("Error List product:", error);
+        res.json({success:false,message:error.message})
+    }
 
 }
 
